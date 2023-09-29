35908540c05d4305ec0205d60e847a1023df07c2
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-09-29 09:15:07 -0500

    chore(technical) Arreglando estructura

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Technical from '../components/Technical.component.vue'
+import Technical from '../components/technical.component.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
