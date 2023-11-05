1558669804a17387943e6b46c451d52c89007bfe
LeonardoFerreyraC <u202111363@upc.edu.pe>
2023-11-05 02:21:36 -0500

    feat(dashboard) Agregando servicio analytics

---
+++ src/services/analytics-api.service.js
@@ -0,0 +1,21 @@
+import http from "@/shared/services/http-common";
+
+export class AnalyticsApiService {
+    getAnalyticsByMonth(month) {
+        return Promise.all([
+            http.get(`/analytics?month=${month}`)
+        ]).then(analyticsResponse => {
+            let data = {};
+            analyticsResponse[0].data.forEach(item => {
+                data[item.week] = {
+                    incomes: item.incomes,
+                    expenses: item.expenses,
+                    profit: item.profit
+                };
+            });
+            return {
+                data
+            };
+        })
+    }
+}
